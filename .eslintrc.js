125ccaa02d6d901494a4c707334ec10a167c605b
TadasCesna-itech <124365963+TadasCesna-itech@users.noreply.github.com>
2023-02-16 15:12:12 +0200

    main setup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    mocha: true,
+    chai: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
